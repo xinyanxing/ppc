e18334fb6b8d5bf0e237f78af1721631d0076886
liuyuxing <1544023393@qq.com>
2019-05-14 19:10:23 +0800

    master change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import { Hello } from './compents/test';
 function App() {
   return (
     <div className="App">
-      sfffssssfdsa nopush:dddd
+      sfffssssfdsa nopush:dddd master
       <Hello
         compiler={['ddd', 'mmmfffmd']}
         framework="ssss"
