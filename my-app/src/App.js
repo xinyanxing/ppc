933df460ac5a989d9456ca90687ef1d45e22be46
liuyuxing <1544023393@qq.com>
2019-05-14 18:59:14 +0800

    gitignore

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Hello } from './compents/test';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello
+        compiler={['ddd', 'mmmfffmd']}
+        framework="ssss"
+        curColor="dddfffffff"
+      />
+    </div>
+  );
+}
+
+export default App;
