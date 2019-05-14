cd177e1bd15f1aad76e225d5e2e678dedd41f034
liuyuxing <1544023393@qq.com>
2019-05-14 19:02:22 +0800

    ssss

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Hello } from './compents/test';
+
+function App() {
+  return (
+    <div className="App">
+      sfffssssfdsa
+      <Hello
+        compiler={['ddd', 'mmmfffmd']}
+        framework="ssss"
+        curColor="dddfffffff"
+      />
+    </div>
+  );
+}
+
+export default App;
